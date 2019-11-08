925cbc5645b6f471bbd3939bf23e75c3c8055ad7
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-11-08 10:23:35 -0500

    Add ThemeContext consumer to Navbar and PageContent for isDarkMode

--- src/PageContent.js
+++ src/PageContent.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import { ThemeContext } from "./contexts/ThemeContext";
 
-export class PageContent extends Component {
+export default class PageContent extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isDarkMode } = this.context;
     const styles = {
-      background: "white",
+      background: isDarkMode ? "black" : "white",
       height: "100vh",
       width: "100vw",
     };
     return <div style={styles}>{this.props.children}</div>;
   }
 }
-
-export default PageContent;
